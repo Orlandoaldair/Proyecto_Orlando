ccda65221c0d40eb535024359bee33504702fb2d
Orlando101-Soriano <orlando101.soso@gmail.com>
2021-01-24 00:08:02 -0600

    initial commit

---
+++ resources/views/votocandidato/list.blade.php
@@ -0,0 +1,50 @@
+@extends('plantilla')
+@section('content')
+<style>
+	.uper {
+		margin-top: 40px;
+}
+</style>
+<div class="uper">
+	@if(session()->get('success'))
+	<div class="alert alert-success">
+		{{ session()->get('success') }}
+	</div><br />
+	@endif
+	<table class="table table-striped">
+	<thead>
+		<tr>
+			<td>voto_id</td>
+			<td>candidato_id</td>
+			<td>votos</td>
+			<td colspan="2">Action</td>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($votocandidatos as $votocandidato)
+			<tr>
+				<td>{{$votocandidato->voto}}</td>
+				<td>{{$votocandidato->candidato}}</td>
+				<td>{{$votocandidato->votos}}</td>
+				<td><a href="{{ route('votocandidato.edit', $votocandidato->id)}}"
+				class="btn btn-primary">Editar</a></td>
+				<td>
+				<form action="{{ route('votocandidato.destroy', $votocandidato->id)}}"
+				method="post">
+				@csrf
+				@method('DELETE')
+				<button class="btn btn-danger" type="submit"
+				onclick="return confirm('Esta seguro de borrar')" >Eliminar</button>
+				</form>
+				</td>
+			</tr>
+		@endforeach
+	</tbody>
+</table>
+<div>
+<p>
+<a href="{{route('votocandidatos.pdf') }}" class="btn btn-sm btn-primary">
+            Descargar Votocandidatos en PDF
+ </a>
+</p>
+@endsection
